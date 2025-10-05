1e5ef1f035fe4abfb7a8608e489a0d4348869c3f
weihao0122 <18520258532@163.com>
2025-10-05 19:56:55 +0800

    theme configuration

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom"
+import { Button } from "antd-mobile"
 const Layout=()=>{
     return (
         <div>
             <Outlet />
             是layout
+            <Button color="primary">全局测试</Button>
+            <div className="purple-theme">
+                <Button color="primary">局部测试</Button>
+            </div>
         </div>
     )
 }
