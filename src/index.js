35630edf606f84cdbb5dddb58c2523fd3ffeaab6
weihao0122 <18520258532@163.com>
2025-10-05 18:45:52 +0800

    别名路径配置

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import sum from '@/test';
+const s=sum(1,5);
+console.log(s);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
