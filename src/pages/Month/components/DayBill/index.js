3ed2e67bbd624fefe02f95ee0e4ecdf42b0568e0
weihao0122 <18520258532@163.com>
2025-10-06 16:16:18 +0800

    Click to toggle between expanded and collapsed states

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -1,23 +1,25 @@
 import classNames from 'classnames'
 import './index.scss'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import { billTypeToName } from '@/contants/index'
+
 const DailyBill = ({date,billList}) => {
-const dayResult= useMemo(()=>{
-    const pay = billList.filter(item=>item.type==='pay').reduce((a,c)=>a+c.money,0)
-    const income = billList.filter(item=>item.type==='income').reduce((a,c)=>a+c.money,0)
-    return {
-        pay,
-        income,
-        total: pay+income
-    }
-  },[billList])
+    const [visible,setVisible]=useState(false)
+    const dayResult= useMemo(()=>{
+        const pay = billList.filter(item=>item.type==='pay').reduce((a,c)=>a+c.money,0)
+        const income = billList.filter(item=>item.type==='income').reduce((a,c)=>a+c.money,0)
+        return {
+            pay,
+            income,
+            total: pay+income
+        }
+    },[billList])
   return (
     <div className={classNames('dailyBill')}>
       <div className="header">
         <div className="dateIcon">
           <span className="date">{date}</span>
-          <span className={classNames('arrow')}></span>
+          <span className={classNames('arrow',visible&&'expand')} onClick={()=>setVisible(!visible)}></span>
         </div>
         <div className="oneLineOverview">
           <div className="pay">
@@ -35,7 +37,7 @@ const dayResult= useMemo(()=>{
         </div>
       </div>
       {/* 单日列表 */}
-    <div className="billList">
+    <div className="billList" style={{display: visible?'block':'none'}}>
     {billList.map(item => {
         return (
         <div className="bill" key={item.id}>
