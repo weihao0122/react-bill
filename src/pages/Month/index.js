be1786fd8a2872475fac70c6248a9e688496fa31
weihao0122 <18520258532@163.com>
2025-10-06 11:19:38 +0800

    Switch time selection box

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,11 +1,56 @@
-
-
-const Month=()=>{
-    return (
-        <div>
-            it is month
+import { NavBar, DatePicker } from 'antd-mobile'
+import './index.scss'
+import { useState } from 'react'
+import classNames from 'classnames'
+const Month = () => {
+  const [dateVisible,setDateVisible]=useState(false)
+  const onConfirm=()=>{
+    setDateVisible(false)
+  }
+  return (
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        月度收支
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 */}
+          <div className="date" onClick={()=>(setDateVisible(true))}>
+            <span className="text">
+              2023 | 3月账单
+            </span>
+            <span className={classNames('arrow', dateVisible &&'expand')}></span>
+          </div>
+          {/* 统计区域 */}
+          <div className='twoLineOverview'>
+            <div className="item">
+              <span className="money">{100}</span>
+              <span className="type">支出</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">收入</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">结余</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className="kaDate"
+            title="记账日期"
+            precision="month"
+            visible={dateVisible}
+            onCancel={()=>{setDateVisible(false)}}
+            onConfirm={()=>onConfirm}
+            onClose={()=>{setDateVisible(false)}}
+            max={new Date()}
+          />
         </div>
-    )
+      </div>
+    </div >
+  )
 }
 
 export default Month
