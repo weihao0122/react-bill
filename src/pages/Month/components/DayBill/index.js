f95f82f0a2548f861750a18f1b26d43ef2928e99
weihao0122 <18520258532@163.com>
2025-10-06 16:05:28 +0800

    Daily List Rendering

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import './index.scss'
 import { useMemo } from 'react'
+import { billTypeToName } from '@/contants/index'
 const DailyBill = ({date,billList}) => {
 const dayResult= useMemo(()=>{
     const pay = billList.filter(item=>item.type==='pay').reduce((a,c)=>a+c.money,0)
@@ -33,6 +34,21 @@ const dayResult= useMemo(()=>{
           </div>
         </div>
       </div>
+      {/* 单日列表 */}
+    <div className="billList">
+    {billList.map(item => {
+        return (
+        <div className="bill" key={item.id}>
+            <div className="detail">
+            <div className="billType">{billTypeToName[item.useFor]}</div>
+            </div>
+            <div className={classNames('money', item.type)}>
+            {item.money.toFixed(2)}
+            </div>
+        </div>
+        )
+    })}
+    </div>
     </div>
   )
 }
