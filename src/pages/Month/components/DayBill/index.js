4fe020492283f83f445104e6815b90d9ccad461b
weihao0122 <18520258532@163.com>
2025-10-06 16:25:15 +0800

    Icon Encapsulation

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -2,6 +2,7 @@ import classNames from 'classnames'
 import './index.scss'
 import { useMemo, useState } from 'react'
 import { billTypeToName } from '@/contants/index'
+import Icon from '@/components/Icon'
 
 const DailyBill = ({date,billList}) => {
     const [visible,setVisible]=useState(false)
@@ -41,6 +42,7 @@ const DailyBill = ({date,billList}) => {
     {billList.map(item => {
         return (
         <div className="bill" key={item.id}>
+            <Icon type={item.useFor}/>
             <div className="detail">
             <div className="billType">{billTypeToName[item.useFor]}</div>
             </div>
